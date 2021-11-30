710fb3c72deecefd76f7e54c3fde1bcace6e71f0
Theo Sandell <theo01sandell@gmail.com>
2021-11-30 17:13:27 +0100

    Added all data to website, added weather icons

--- script/index.js
+++ script/index.js
@@ -1,14 +1,14 @@
+let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 const current = {
   wrapper: document.querySelector('section.weather'),
-  icon: document.querySelector('section.weather .icon'),
+  icon: document.querySelector('section.weather .icon i'),
   location: document.querySelector('section.weather .location'),
-  temp: document.querySelector('section.weather .current'),
-  highLow: document.querySelector('section.weather .high-low'),
+  temp: document.querySelector('section.weather .current-temp'),
+  weatherText: document.querySelector('section.weather .weather-text'),
 };
+const forecastsWrapper = document.querySelector('.forecast');
 
-const forecastWrapper = document.querySelector('.forecast');
-
-console.log(current);
+console.log(icons);
 
 const getAntipode = (lat, lon) => {
   let antipodes = [];
@@ -22,6 +22,37 @@ const getAntipode = (lat, lon) => {
   }
 
   return antipodes;
+};
+
+const translateMonth = (month) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'November',
+    'December',
+  ];
+
+  return months[month - 1];
+};
+
+const getWeatherIcon = (code) => {
+  const prefix = 'wi wi-';
+  let icon = icons[code].icon;
+
+  // If we are not in the ranges mentioned above, add a day/night prefix.
+  if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+    icon = 'day-' + icon;
+  }
+
+  // Finally tack on the prefix.
+  return prefix + icon;
 };
 
 const getCoords = () => {
@@ -68,7 +99,55 @@ getCoords()
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        current.temp.textContent = Math.floor(data.current.temp) + '°C';
+        current.weatherText.textContent = data.current.weather[0].description;
+        current.location.textContent =
+          data.closest_city.name +
+          ', ' +
+          regionNames.of(data.closest_city.countryCode);
+        current.icon.classList = getWeatherIcon(data.current.weather[0].id);
+
+        data.daily.forEach((forecast) => {
+          const forecastWrapper = document.createElement('div');
+          const forecastIcon = document.createElement('div');
+          const forecastIconText = document.createElement('i');
+          const weatherInfo = document.createElement('div');
+          const date = document.createElement('h4');
+          const highLowWrapper = document.createElement('div');
+          const high = document.createElement('p');
+          const low = document.createElement('p');
+
+          const forecastDate = new Date(forecast.dt * 1000);
+
+          forecastWrapper.classList.add('single-forecast');
+          forecastIcon.classList.add('forecast-icon');
+          weatherInfo.classList.add('forecast-info');
+          date.classList.add('forecast-date');
+          highLowWrapper.classList.add('forecast-high-low');
+          high.classList.add('forecast-high');
+          low.classList.add('forecast-low');
+
+          forecastIcon.appendChild(forecastIconText);
+          weatherInfo.appendChild(date);
+          weatherInfo.appendChild(highLowWrapper);
+          highLowWrapper.appendChild(high);
+          highLowWrapper.appendChild(low);
+
+          forecastWrapper.appendChild(forecastIcon);
+          forecastWrapper.appendChild(weatherInfo);
+
+          forecastIcon.classList = getWeatherIcon(forecast.weather[0].id);
+          date.textContent =
+            forecastDate.getDate() +
+            ' ' +
+            translateMonth(forecastDate.getMonth()) +
+            ' ' +
+            forecastDate.getFullYear();
+          high.textContent = 'High: ' + Math.floor(forecast.temp.max) + '°C';
+          low.textContent = 'Low: ' + Math.floor(forecast.temp.min) + '°C';
+
+          forecastsWrapper.append(forecastWrapper);
+        });
       });
   })
   .catch((error) => {
